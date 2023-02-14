940346c14dd0a14b805324f73f74f404ea8c94b3
Prince Singh <thisisprincesingh@gmail.com>
2023-02-14 19:52:25 +0530

    Add files via upload
    add all files

---
+++ Greedy Algrothim in Java/src/GreedyAlgorithm/KnapsackProblem.java
@@ -0,0 +1,8 @@
+package GreedyAlgorithm;
+
+public class KnapsackProblem {
+    public static void main(String[] args) {
+
+        System.out.println("Knapsack Problem");
+    }
+}
---
+++ Greedy Algrothim in Java/src/GreedyAlgorithm/Main.java
@@ -0,0 +1,8 @@
+package GreedyAlgorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Gready Algorithm In Java");
+    }
+}
